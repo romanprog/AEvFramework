f9930581147fa66029b0056e7d92b7ad9db5d7d8
Roman Progonnyj <roman.progonnyj@gmail.com>
2017-05-04 11:03:03 +0300

    Restruct

---
+++ Server/Listener.cpp
@@ -0,0 +1,7 @@
+#include "Listener.hpp"
+
+Listener::Listener()
+{
+
+}
+
---
+++ Server/Listener.hpp
@@ -0,0 +1,15 @@
+#ifndef LISTENER_H
+#define LISTENER_H
+
+#include "../AsyncEvent/AEvBase/AEventAbstract.hpp"
+
+
+class Listener : public aev::AEventAbstract
+{
+public:
+    Listener();
+};
+
+
+
+#endif // LISTENER_H
